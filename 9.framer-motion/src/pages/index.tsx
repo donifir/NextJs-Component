2043ff8d2ce836b167db17b7bcf0ba798189b027
donifir <didonifirmansyah@gmail.com>
2023-08-09 11:58:23 +0700

    onscroll

--- 9.framer-motion/src/pages/index.tsx
+++ 9.framer-motion/src/pages/index.tsx
@@ -1,7 +1,24 @@
-import React from 'react'
-import { motion } from "framer-motion"
+import React, { useRef } from 'react'
+import { motion , Variants} from "framer-motion"
+
+const cardVariants: Variants = {
+  offscreen: {
+    y: 300
+  },
+  onscreen: {
+    y: 50,
+    rotate: -10,
+    transition: {
+      type: "spring",
+      bounce: 0.4,
+      duration: 2
+    }
+  }
+};
+
 
 const index = () => {
+  const scrollRef = useRef(null)
   return (
     <div className='flex flex-col justify-center items-center gap-6 py-10'>
       <motion.div
@@ -49,6 +66,40 @@ const index = () => {
             <button className="btn btn-primary">Buy Now</button>
           </div>
         </div>
+      </motion.div>
+      <motion.div
+        animate={{ rotate: 20 }}
+        className="card w-96 bg-base-100 shadow-xl">
+        <div className="card-body">
+          <h2 className="card-title">Shoes!</h2>
+          <p>If a dog chews shoes whose shoes does he choose?</p>
+          <div className="card-actions justify-end">
+            <button className="btn btn-primary">Buy Now</button>
+          </div>
+        </div>
+      </motion.div>
+
+      {/* onscroll */}
+      <motion.div
+        className="card-container"
+        initial="offscreen"
+        whileInView="onscreen"
+        viewport={{ once: true, amount: 0.8 }}
+      >
+        <div className="splash" />
+        <motion.div className="card" variants={cardVariants}>
+          <motion.div
+            animate={{ rotate: 20 }}
+            className="card w-96 bg-base-100 shadow-xl">
+            <div className="card-body">
+              <h2 className="card-title">Shoes!</h2>
+              <p>If a dog chews shoes whose shoes does he choose?</p>
+              <div className="card-actions justify-end">
+                <button className="btn btn-primary">Buy Now</button>
+              </div>
+            </div>
+          </motion.div>
+        </motion.div>
       </motion.div>
     </div>
   )
